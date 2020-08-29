0c7e39a9c703f800357afcff0af75c9fbf9c98ad
Devansh0210 <devansht0210@gmail.com>
2020-08-29 17:38:02 +0530

    dependecies included

--- setup.py
+++ setup.py
@@ -4,7 +4,8 @@ setup(
     name='binCircuit',
     version='1.0',
     py_modules=['all','graycode','parserBool','printer'],
-    install_packages=['Click','numpy'],
+    install_packages=['click','numpy'],
+    install_requires=['click','numpy'],
     entry_points='''
         [console_scripts]
         bincircuit=all:cli
